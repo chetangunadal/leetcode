0643581e15e7923c84c31c8d02ef251a4295d18c
chetangunadal <chetangunadal04@gmail.com>
2026-09-12 20:12:43 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (51.91%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.c
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.c
@@ -6,28 +6,26 @@
  * };
  */
 struct ListNode* swapPairs(struct ListNode* head) {
-    if (head==0 || head->next==0)
+    if (head == 0 || head->next == 0)
+        return head;
+    struct ListNode *first = head, *sec = head->next, *prve = 0, *third;
+
+    while (sec && first) {
+        third = sec->next;
+        sec->next = first;
+        first->next = third;
+
+        if (prve)
+            prve->next = sec;
+        else
+            head = sec;
+
+        prve = first;
+        first = third;
+        if (third)
+            sec = third->next;
+        else
+            sec = 0;
+    }
     return head;
-struct ListNode *first=head,*sec=head->next,*prve=0,*third;
-
- while(sec && first)
-    {
-        third=sec->next;
-        sec->next=first;
-        first->next=third;
-
-        if(prve==0)
-        head=sec;
-        else
-        prve->next=sec;
-
-        prve=first;
-        first=third;
-        if(third)
-        sec=third->next;
-        else
-        sec=0;
-       
-    }    
-return head;
 }
