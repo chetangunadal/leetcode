c363cedfafc92abc93929f471c4de2b9cfdd2bd9
chetangunadal <chetangunadal04@gmail.com>
2026-09-15 21:28:45 +0530

    Time: 3 ms (43.86%) | Memory: 195.2 MB (23.48%) - LeetSync

--- 189-rotate-array/rotate-array.c
+++ 189-rotate-array/rotate-array.c
@@ -2,16 +2,19 @@ void rotate(int* nums, int numsSize, int k) {
     k = k % numsSize;
     int i = 0, a[100000], l = numsSize - k;
 
-    while (i < l) {
-        a[i] = nums[i];
-        i++;
-    }
+    
 
     i = 0;
-    while (i < k) {
+    while ((i < k)||(i<l)) {
+        if(i<l){
+         a[i] = nums[i];
+        }
+        if(i < k){
         nums[i] = nums[l + i];
+        }
         i++;
     }
+    i=k;
     int t = 0;
     while (i < numsSize) {
         nums[i] = a[t];
